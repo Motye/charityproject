6e2c2dad8c46b50d0ccd84974972b0edc8c8094e
Dave Weiner <david.l.weiner30030@gmail.com>
2017-07-04 21:03:14 -0400

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>The Midshipman Elijah Dimas Project</title>
+
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<div class="container project-container">
+    <header class="header">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="images/forumheader.png">
+            </div>
+            <div class="col-sm-6 header-vert-center">
+                <h1 class="text-uppercase">The Midshipman Elijah Dimas Project</h1>
+            </div>
+        </div>
+    </header>
+
+    <div>
+        <h1 class="text-center text-uppercase">Standing With a Hero</h1>
+
+        <p>Elijah Dimas (HMS Implacable the Sante Fe chapter of The Royal Manticoran Navy, the Official Honor
+            Harrington Fan Club) is the RMN's youngest Midshipman. 9 years old, entering 5th grade, Elijah is fond
+            of math, science, and especially art. He is an avid fan of science-fiction (especially David Weber's
+            Honor Harrington series), Pokémon, and the Oakland Raiders. He loves elephants, ventriloquism, jokes,
+            the colors green and yellow, and he particularly enjoys helping other children face their own
+            challenges.</p>
+
+        <p>Elijah lives his life according the quote "Live Your Life Fearlessly!"</p>
+
+        <p>How profound a quote, when you realize this amazing young man was diagnosed with Ewing's Sarcoma (a rare,
+            debilitating bone cancer) at the age of 7 and given only two weeks to live. Here he is, two years later,
+            defying the odds and fighting on in a way too few of us could ever hope to emulate.</p>
+
+        <img class="elijah img-responsive" alt="Elijah reading OSB" src="images/elijah.jpg" />
+        <p>32 cycles of chemotherapy. 15 surgical procedures. Over 70 tumors at initial diagnosis. In the two weeks
+            from when they were all eliminated through aggressive chemotherapy, 9 of them stubbornly returned before
+            he could undergo stem cell transplant therapy.</p>
+
+        <p>Elijah Dimas has every reason to be down, depressed, and angry at the world. Instead, he insists on
+            visiting hospitals the very place he should reasonably hate! during the holidays to cheer up other
+            children! He is always smiling, always positive, and goes out of his way to make his own challenges
+            easier on his family, friends, and supporters. He wants to live, and he wants to dedicate his life to
+            helping others as a child life specialist.</p>
+
+        <p>Elijah is a hero to everyone who knows him, and he's been fully adopted by the crew of HMS Implacable and
+            the entire global community that is the Royal Manticoran Navy. His is the example we all strive to
+            emulate, and now we want to step up and aid him and his family in every way we can.</p>
+
+        <p>None are more dedicated to this than the grand author of the Honor Harrington series himself, David
+            Weber. David has very kindly offered up a slot on his highly coveted First Readers List to the highest
+            bidder of this special auction.</p>
+
+        <p>This is an exclusive, closed list of individuals who receive advanced copies of David's manuscripts at
+            the same time they are sent to the publisher, and long before anyone else ever gets to see the work.
+            First readers see the work before any copy edit, giving them a chance to read it before anyone else and
+            to see how the editing process can change between submission and final printing.</p>
+
+        <p>The highest bidder for this charitable auction will be helping to defray the enormous expenses faced by
+            Elijah's family as they continue to struggle day-to-day to make ends meet while taking care of their
+            son. His treatments take him across the country, with frequent trips to Denver's Children's Hospital and
+            as far as the Cleveland Clinic in Ohio. These trips and all the attendant costs are a severe hardship
+            for these exceptional people; Elijah's mother, Patricia, quit her job to care for him full time, and his
+            father, Anthony, works hard to support the whole family (often missing the trips in order to keep the
+            core income flowing).</p>
+
+        <p>This is your chance to join a highly exclusive club and be among the very privileged few who get to see
+            the works of David Weber long before anyone else.</p>
+
+        <p>It's also your opportunity to stand with a true hero, young of age and small of stature, but a giant of
+            spirit and bravery who inspires us all.</p>
+
+        <p>Thank you.</p>
+    </div>
+</div>
+</body>
+</html>
