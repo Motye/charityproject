fb60777c39084f3743ea651ea586df3c7be491f8
Dave Weiner <david.l.weiner30030@gmail.com>
2017-07-12 18:23:37 -0400

    Don't prompt if bidding isn't open

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
             </div>
         @endif
 
-        @if(Auth::check() && Auth::user()->confirmed === true && (time() >= strtotime(config('bids.open') || config('bids.skip_date_check') === true)))
+        @if((time() >= strtotime(config('bids.open')) || config('bids.skip_date_check') === true) && (Auth::user()->confirmed === true && Auth::check()))
             @if(Auth::user()->numberOfBids() === 0)
                 <div class="alert alert-success alert-dismissable" role="alert">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
